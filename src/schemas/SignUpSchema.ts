a6b3cc38e87e3d48a72b96f57f069481287952c0
MeznahAldossari <MeznahAldossari@hotmail.com>
2024-10-07 09:15:15 +0300

    v3

--- src/schemas/SignUpSchema.ts
+++ src/schemas/SignUpSchema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const formSchema = z.object({
     userName: z.string().trim().min(6, {message:'Username should be contains at least 6 character'}),
     email: z.string().trim().email('Email is Invalid'),
-    password: z.string().trim().min(7, {message:'Password should be contains at least 7 character'}).regex(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{7,16}$/,{message: 'Password shound be contains at least one Capital and Small Charatcer and One Digit and Special Symbol'}),
+    password: z.string().trim().min(7, {message:'Password should be contains at least 7 (letter, symbol, number'}).regex(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{7,16}$/,{message: 'Password shound be contains at least one Capital and Small Charatcer and One Digit and Special Symbol'}),
   })
   
 export type User = z.infer<typeof formSchema>
