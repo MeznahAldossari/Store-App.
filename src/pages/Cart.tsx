dfaee8fd542940d89eef713639b121d68ce593b2
MeznahAldossari <MeznahAldossari@hotmail.com>
2024-10-07 09:59:42 +0300

    v6

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -49,7 +49,7 @@ function Cart() {
   const updateTotal = ():void =>{
     let getUser:string|null = localStorage.getItem("userID")
     axios.get(`https://667b1a30bd627f0dcc91b421.mockapi.io/Users/allUsers/${getUser}`).then((res)=>{
-     
+
       let result:number = res.data.cartItem?.reduce(
         (accumulator:number, item:cartInfo) => {
           let getNewQty = parseInt(qtyValue[item.id]) || item.qty
@@ -76,7 +76,6 @@ function Cart() {
     axios.get(`https://667b1a30bd627f0dcc91b421.mockapi.io/Users/allUsers/${getUser}`).then((res)=>{
 
       let newList:cartInfo[] = res.data.cartItem.filter((_item:cartInfo, index:number) => index !== selectedID)
-      console.log("Hello 1000000");
       axios.put(`https://667b1a30bd627f0dcc91b421.mockapi.io/Users/allUsers/${getUser}`,{
         cartItem:[...newList]
       }).then((_response)=>{
@@ -168,75 +167,7 @@ function Cart() {
         </section>
       )}
     </>
-    // <main>
-    //   <Nav cartTotal={itemsLength}/>
-    //   <section className="flex lg:grid lg:grid-cols-3 lg:gap-x-10 lg:py-6 max-sm:flex-col max-sm:py-12 max-sm:px-6 px-10 lg:px-12">
-    //     <section className="flex justify-center lg:col-span-2  overflow-y-auto lg:h-[75vh]">
-    //   {itemDetails?.length ?(
-    //      <table className="table-fixed h-fit w-full  border-[1.5px] shadow-lg border-gray rounded-md">
-    //      <tr className="bg-gray-300">
-    //        <th>Image</th>
-    //        <th>Name</th>
-    //        <th>Size</th>
-    //        <th>Price</th>
-    //        <th>Quantity</th>
-    //        <th>Delete</th>
-           
-    //      </tr>
-    //      {itemDetails?.map((item,index)=>(
-    //        <tr key={index} className=" border-[1.5px] border-gray">
-    //          <td className="border border-white  flex justify-center text-wrap px-4 py-2 text-center max-sm:px-0"><img src={item?.images} className="max-sm:w-10 max-sm:h-16 w-[6rem] h-[6rem]"></img></td>
-    //          <td className="border border-white  text-wrap px-4 py-2 text-center max-sm:px-0" >{item.title}</td>
-    //          <td className="border border-white  text-wrap px-4 py-2 text-center max-sm:px-0">
-    //             {item.size? (item.size): "-"}
-    //          </td>
-    //          <td className="border border-white  text-wrap px-4 py-2 text-center max-sm:px-0">{item.price}</td>
-    //          <td className="border border-white  text-wrap px-4 py-2 text-center max-sm:px-0">
-    //             <select key={item.id} value={qtyValue[item.id] || item.qty} onChange={(e)=>{setQtyValue((prev)=>({...prev, [item.id]:e.target.value}))}} className="px-2 border-[1.2px] border-gray-400 rounded-md">
-    //                     <option value="1">1</option>
-    //                     <option value="2">2</option>
-    //                     <option value="3">3</option>
-    //             </select>
-    //          </td>
-    //          <td className="text-center">
-    //          <button className="btn m-auto bg-white border-none" onClick={()=> deleteByID(index)}><img src={deleteIcon}  className='lg:w-4 w-4 cursor-pointer'></img></button> 
-    //          <dialog id="my_modal_2" className="modal">
-    //           <div className="modal-box">
-                  
-    //               <p className="py-4">Are you sure you want to remove this Product?</p>
-    //               <div className="modal-action">
-    //               <form method="dialog" >
-    //                   <button className="btn mx-2 bg-red-500 text-white " onClick={()=> deleteItem(deleteBox)}>yes</button>
-    //                   <button className="btn">No</button>
-
-    //             </form>
-    //               </div>
-    //           </div>
-    //           </dialog>
-                        
-
-    //          </td>
-    //          </tr>
-    //      ))}
-         
-    //    </table>
-
-
-    //   ):(
-    //     <p className="text-[1.2em] text-gray-500 font-semibold text-center py-6">There are No Products in Here</p>
-    //   )}
-         
-    //     </section>
-    //     <section className="flex lg:h-fit lg:py-4 px-2 rounded-md shadow-lg border-[1.2px] border-gray-300  flex-col max-ms:gap-y-3 max-sm:py-1 max-sm:mt-4  pb-2">
-          
-    //           <p className="flex justify-between py-4 max-sm:pb-7 font-bold"> <span>Total:</span><span className="text-red-500">{total}$</span></p>
-    //           <button className="bg-[#6146cb] hover:bg-[#6141e0] rounded-sm py-1 text-white w-full">Checkout</button>
-        
-    //     </section>
-
-    //   </section>
-    //  <Footer/>
-    // </main>
+   
   )
 }
 
